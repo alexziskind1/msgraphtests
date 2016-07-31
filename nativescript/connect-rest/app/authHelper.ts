d0623653536e42a4c0f0bf02649a59d8a3426fb3
Alexander Ziskind <alex@nuvious.com>
2016-07-31 07:47:00 -0400

    Added connect-rest sample projects

---
+++ nativescript/connect-rest/app/authHelper.ts
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+var OAuth = require('oauth');
+var uuid = require('node-uuid');
+
+// The application registration (must match Azure AD config)
+var credentials = {
+  authority: 'https://login.microsoftonline.com/common',
+  authorize_endpoint: '/oauth2/v2.0/authorize',
+  token_endpoint: '/oauth2/v2.0/token',
+  client_id: 'fe3ebdc0-f108-4cea-9757-95cc0638787e',
+  client_secret: 'DHyKa8sr04Xop5hPtiYWKiE',
+  redirect_uri: 'urn:ietf:wg:oauth:2.0:oob',
+  scope: 'Files.ReadWrite User.Read Mail.Send offline_access'
+};
+
+/**
+ * Generate a fully formed uri to use for authentication based on the supplied resource argument
+ * @return {string} a fully formed uri with which authentication can be completed
+ */
+function getAuthUrl() {
+  return credentials.authority + credentials.authorize_endpoint +
+    '?client_id=' + credentials.client_id +
+    '&response_type=code' +
+    '&redirect_uri=' + credentials.redirect_uri +
+    '&scope=' + credentials.scope +
+    '&response_mode=query' +
+    '&nonce=' + uuid.v4() +
+    '&state=abcd';
+}
+
+/**
+ * Gets a token for a given resource.
+ * @param {string} code An authorization code returned from a client.
+ * @param {AcquireTokenCallback} callback The callback function.
+ */
+function getTokenFromCode(code, callback) {
+  var OAuth2 = OAuth.OAuth2;
+  var oauth2 = new OAuth2(
+    credentials.client_id,
+    credentials.client_secret,
+    credentials.authority,
+    credentials.authorize_endpoint,
+    credentials.token_endpoint
+  );
+
+  oauth2.getOAuthAccessToken(
+    code,
+    {
+      grant_type: 'authorization_code',
+      redirect_uri: credentials.redirect_uri,
+      response_mode: 'form_post',
+      nonce: uuid.v4(),
+      state: 'abcd'
+    },
+    function (e, accessToken, refreshToken) {
+      callback(e, accessToken, refreshToken);
+    }
+  );
+}
+
+
+/**
+ * Gets a new access token via a previously issued refresh token.
+ * @param {string} refreshToken A refresh token returned in a token response
+ *                       from a previous result of an authentication flow.
+ * @param {AcquireTokenCallback} callback The callback function.
+ */
+function getTokenFromRefreshToken(refreshToken, callback) {
+  var OAuth2 = OAuth.OAuth2;
+  var oauth2 = new OAuth2(
+    credentials.client_id,
+    credentials.client_secret,
+    credentials.authority,
+    credentials.authorize_endpoint,
+    credentials.token_endpoint
+  );
+
+  oauth2.getOAuthAccessToken(
+    refreshToken,
+    {
+      grant_type: 'refresh_token',
+      redirect_uri: credentials.redirect_uri,
+      response_mode: 'form_post',
+      nonce: uuid.v4(),
+      state: 'abcd'
+    },
+    function (e, accessToken) {
+      callback(e, accessToken);
+    }
+  );
+}
+
+exports.credentials = credentials;
+exports.getAuthUrl = getAuthUrl;
+exports.getTokenFromCode = getTokenFromCode;
+exports.getTokenFromRefreshToken = getTokenFromRefreshToken;
+exports.ACCESS_TOKEN_CACHE_KEY = 'ACCESS_TOKEN_CACHE_KEY';
+exports.REFRESH_TOKEN_CACHE_KEY = 'REFRESH_TOKEN_CACHE_KEY';
