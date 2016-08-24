246ef218d16b5645b8e48f8d5acbf91723e6f579
Alexander Ziskind <alex@nuvious.com>
2016-08-24 08:40:46 -0400

    upgraded to latest nativescript-oauth plugin

--- nativescript/msgraphoauthplugintest/app/app.ts
+++ nativescript/msgraphoauthplugintest/app/app.ts
@@ -4,7 +4,7 @@ import * as navigationModule from './shared/navigation';
 import * as constantsModule from './shared/constants';
 
 
-var o365InitOptions : TnsOAuth.ITnsOAuthOptionsOffice365 = {
+var o365InitOptions : tnsOAuthModule.ITnsOAuthOptionsOffice365 = {
     clientId: constantsModule.CLIENT_ID, //client id for application (GUID) 
     scope: constantsModule.ACTIVE_SCOPES //whatever other scopes you need 
 };
